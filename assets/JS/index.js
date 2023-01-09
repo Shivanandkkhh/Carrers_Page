d56e01a7b2b18cc60e783e44c1b32992cb6a2b70
Shivanandkkhh <Shivanand@shoptrade.co>
2023-01-09 11:25:22 +0530

    buttons adjusted

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,3 +1,5 @@
+
+// Js for switching the job postions (tablinks)
 const tabs = document.querySelectorAll('.tablinks');
 const tabcontent = document.querySelectorAll('.position-grid');
 for (let i = 0; i < tabs.length; i++) {
@@ -10,5 +12,22 @@ for (let i = 0; i < tabs.length; i++) {
         }
         tabcontent[i].classList.add('tab-content-active');
         tabs[i].classList.add('active');
+    })
+}
+
+// Testimonials sliding property?
+
+const tabsTest = document.querySelectorAll('.tablinks');
+const tabcontentTest = document.querySelectorAll('.position-grid');
+for (let i = 0; i < tabsTest.length; i++) {
+    tabsTest[i].addEventListener("click", () => {
+        for (let j = 0; j < tabcontentTest.length; j++) {
+            tabcontentTest[j].classList.remove('tab-content-active');
+        }
+        for (let k = 0; k < tabsTest.length; k++) {
+            tabsTest[k].classList.remove('active');
+        }
+        tabcontentTest[i].classList.add('tab-content-active');
+        tabsTest[i].classList.add('active');
     })
 }
